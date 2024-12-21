a781fa005ab218f90c7a31ed781de802c7e16bbb
Eekshit Chawla <eekshitchawla@eeki.local>
2024-12-21 15:50:19 +0530

    UI bettermwnt

--- src/screens/BodyMain/BodyMain.js
+++ src/screens/BodyMain/BodyMain.js
@@ -1,55 +1,96 @@
+// import React from "react";
+// import "../BodyMain/BodyMain.css";
+// // import "../Signup/Signup.css";
+
+// const BodyMain = () => {
+//   return (
+//     <div id="mainBody">
+//       <div id="mainBodyContent">
+//         <h1>WELCOME TO SKILL SELECT</h1>
+//         <p style={{ fontSize: "20px" }}>
+//           Welcome to SKILL SELECT, your ultimate destination for online hiring!
+//           We understand that finding the right talent or securing the perfect
+//           opportunity can be a challenging task. That's why we have created a
+//           platform that brings together job seekers and employers in a seamless
+//           and efficient manner. Whether you are a student looking for part-time
+//           work or a professional seeking a new career path, SKILL SELECT is here
+//           to help you succeed. With our extensive database of job listings,
+//           user-friendly interface, and powerful search tools, you can easily
+//           navigate through a vast array of opportunities and connect with
+//           potential employers. Join SKILL SELECT today and embark on a journey
+//           towards your dream job!
+//         </p>
+//       </div>
+//       <div id="featureContent" style={{ fontSize: "18px" }}>
+//         <h1>FEATURES</h1>
+//         <ul style={{ listStyleType: "none" }}>
+//           <li style={{ padding: "1vh" }}>
+//             Comprehensive Job Listings: Our platform provides an extensive
+//             database of job listings across various industries and sectors. From
+//             entry-level positions to executive roles, you'll find a wide array
+//             of opportunities to explore.
+//           </li>
+//           <li style={{ padding: "1vh" }}>
+//             Advanced Search Filters: Save time and find the perfect job or
+//             candidate with our advanced search filters. Refine your search based
+//             on location, salary range, experience level, and more, ensuring you
+//             find the most relevant options.
+//           </li>
+//           <li style={{ padding: "1vh" }}>
+//             Resume/CV Upload: Seamlessly upload your resume or CV to showcase
+//             your qualifications and stand out from the crowd. Employers can
+//             easily review applicant profiles and make informed hiring decisions.
+//           </li>
+//           <li style={{ padding: "1vh" }}>
+//             Profile Creation: Build a compelling profile that highlights your
+//             skills, qualifications, and experience. Job seekers can showcase
+//             their expertise, while employers can create a company profile to
+//             attract top talent.
+//           </li>
+//         </ul>
+//       </div>
+//       <div id="whiteBox"></div>
+//     </div>
+//   );
+// };
+
+// export default BodyMain;
 import React from "react";
 import "../BodyMain/BodyMain.css";
-// import "../Signup/Signup.css";
 
 const BodyMain = () => {
   return (
     <div id="mainBody">
+      <div id="whiteBox"></div>
       <div id="mainBodyContent">
         <h1>WELCOME TO SKILL SELECT</h1>
-        <p style={{ fontSize: "20px" }}>
+        <p>
           Welcome to SKILL SELECT, your ultimate destination for online hiring!
           We understand that finding the right talent or securing the perfect
           opportunity can be a challenging task. That's why we have created a
           platform that brings together job seekers and employers in a seamless
-          and efficient manner. Whether you are a student looking for part-time
-          work or a professional seeking a new career path, SKILL SELECT is here
-          to help you succeed. With our extensive database of job listings,
-          user-friendly interface, and powerful search tools, you can easily
-          navigate through a vast array of opportunities and connect with
-          potential employers. Join SKILL SELECT today and embark on a journey
+          and efficient manner. Join SKILL SELECT today and embark on a journey
           towards your dream job!
         </p>
       </div>
-      <div id="featureContent" style={{ fontSize: "18px" }}>
+      <div id="featureContent">
         <h1>FEATURES</h1>
-        <ul style={{ listStyleType: "none" }}>
-          <li style={{ padding: "1vh" }}>
-            Comprehensive Job Listings: Our platform provides an extensive
-            database of job listings across various industries and sectors. From
-            entry-level positions to executive roles, you'll find a wide array
-            of opportunities to explore.
+        <ul>
+          <li>
+            <span>🔍</span> <strong>Advanced Search Filters:</strong> Save time
+            and find the perfect job or candidate with our advanced search
+            filters.
           </li>
-          <li style={{ padding: "1vh" }}>
-            Advanced Search Filters: Save time and find the perfect job or
-            candidate with our advanced search filters. Refine your search based
-            on location, salary range, experience level, and more, ensuring you
-            find the most relevant options.
-          </li>
-          <li style={{ padding: "1vh" }}>
-            Resume/CV Upload: Seamlessly upload your resume or CV to showcase
-            your qualifications and stand out from the crowd. Employers can
-            easily review applicant profiles and make informed hiring decisions.
-          </li>
-          <li style={{ padding: "1vh" }}>
-            Profile Creation: Build a compelling profile that highlights your
-            skills, qualifications, and experience. Job seekers can showcase
-            their expertise, while employers can create a company profile to
-            attract top talent.
+          {/* <li>
+            <span>📂</span> <strong>Resume/CV Upload:</strong> Seamlessly upload
+            your resume or CV to showcase your qualifications.
+          </li> */}
+          <li>
+            <span>👤</span> <strong>Profile Creation:</strong> Build a
+            compelling profile to highlight your skills and experience.
           </li>
         </ul>
       </div>
-      <div id="whiteBox"></div>
     </div>
   );
 };
--- src/screens/Navbar/Navbar.js
+++ src/screens/Navbar/Navbar.js
@@ -1,107 +1,310 @@
+// import React, { useEffect, useState } from "react";
+// import "../Navbar/Navbar.css";
+// import { doc, getDoc } from "firebase/firestore";
+// import { Firebase } from "../../utils/firebase";
+// import logo from "../../assets/logo.png";
+// const Navbar = () => {
+//   const [email, setEmail] = useState(localStorage.getItem("email"));
+//   const [username, setUsername] = useState("");
+//   const db = Firebase();
+
+//   useEffect(() => {
+//     const displayUser = async () => {
+//       if (email) {
+//         const docSnap = await getDoc(doc(db, "users", email));
+//         if (docSnap.exists()) {
+//           setUsername(docSnap.data().name.toUpperCase());
+//         }
+//       }
+//     };
+//     displayUser();
+//   }, [email, db, username]);
+
+//   const logout = () => {
+//     localStorage.clear();
+//     setEmail(null);
+//   };
+
+//   return (
+//     <div id="navbarParent">
+//       <nav id="navBar">
+//         <link
+//           rel="stylesheet"
+//           href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css"
+//           integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm"
+//           crossOrigin="anonymous"
+//         />
+//         <div id="container">
+//           <button
+//             id="appName"
+//             onClick={() => {
+//               window.location.href = "/";
+//             }}
+//           >
+//             <img src={logo} alt="" />
+//           </button>
+//           <div className="nameRender" id="itemBtn">
+//             {username === "" ? "" : `HEY ${username} !`}
+//           </div>
+//           <div id="navbarLinks">
+//             <ul id="list">
+//               <li id="item">
+//                 <button
+//                   id="itemBtn"
+//                   onClick={() => {
+//                     window.location.href = "/myList";
+//                   }}
+//                 >
+//                   Shortlisted
+//                 </button>
+//               </li>
+//               <li id="item">
+//                 <button
+//                   id="itemBtn"
+//                   onClick={() => {
+//                     window.location.href = "/candidates";
+//                   }}
+//                 >
+//                   All Candidates
+//                 </button>
+//               </li>
+//               <li id="item">
+//                 {email ? (
+//                   <button
+//                     id="itemBtn"
+//                     onClick={() => {
+//                       logout();
+//                     }}
+//                   >
+//                     Logout
+//                   </button>
+//                 ) : (
+//                   <button
+//                     id="itemBtn"
+//                     onClick={() => {
+//                       window.location.href = "/login";
+//                     }}
+//                   >
+//                     Login
+//                   </button>
+//                 )}
+//               </li>
+//               {email ? (
+//                 ""
+//               ) : (
+//                 <li id="item">
+//                   <button
+//                     id="itemBtn"
+//                     onClick={() => {
+//                       window.location.href = "/signup";
+//                     }}
+//                   >
+//                     Signup
+//                   </button>
+//                 </li>
+//               )}
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
+// import React, { useEffect, useState } from "react";
+// import "../Navbar/Navbar.css";
+// import { doc, getDoc } from "firebase/firestore";
+// import { Firebase } from "../../utils/firebase";
+// import logo from "../../assets/logo.png";
+
+// const Navbar = () => {
+//   const [email, setEmail] = useState(localStorage.getItem("email"));
+//   const [username, setUsername] = useState("");
+//   const db = Firebase();
+
+//   useEffect(() => {
+//     const fetchUser = async () => {
+//       if (email) {
+//         try {
+//           const docSnap = await getDoc(doc(db, "users", email));
+//           if (docSnap.exists()) {
+//             setUsername(docSnap.data().name.toUpperCase());
+//           }
+//         } catch (error) {
+//           console.error("Error fetching user data:", error);
+//         }
+//       }
+//     };
+//     fetchUser();
+//   }, [email, db]);
+
+//   const handleLogout = () => {
+//     localStorage.clear();
+//     setEmail(null);
+//     setUsername("");
+//     window.location.href = "/";
+//   };
+
+//   return (
+//     <div id="navbarParent">
+//       <nav id="navBar">
+//         <div id="container">
+//           <button id="appName" onClick={() => (window.location.href = "/")}>
+//             <img src={logo} alt="Skill Select Logo" />
+//           </button>
+//           <div id="userGreeting" className="nameRender">
+//             {username && `Hey, ${username}!`}
+//           </div>
+//           <div id="navbarLinks">
+//             <ul id="list">
+//               <li>
+//                 <button
+//                   id="itemBtn"
+//                   onClick={() => (window.location.href = "/myList")}
+//                 >
+//                   Shortlisted
+//                 </button>
+//               </li>
+//               <li>
+//                 <button
+//                   id="itemBtn"
+//                   onClick={() => (window.location.href = "/candidates")}
+//                 >
+//                   All Candidates
+//                 </button>
+//               </li>
+//               {email ? (
+//                 <li>
+//                   <button id="itemBtn" onClick={handleLogout}>
+//                     Logout
+//                   </button>
+//                 </li>
+//               ) : (
+//                 <>
+//                   <li>
+//                     <button
+//                       id="itemBtn"
+//                       onClick={() => (window.location.href = "/login")}
+//                     >
+//                       Login
+//                     </button>
+//                   </li>
+//                   <li>
+//                     <button
+//                       id="itemBtn"
+//                       onClick={() => (window.location.href = "/signup")}
+//                     >
+//                       Signup
+//                     </button>
+//                   </li>
+//                 </>
+//               )}
+//             </ul>
+//           </div>
+//         </div>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Navbar;
+
 import React, { useEffect, useState } from "react";
 import "../Navbar/Navbar.css";
 import { doc, getDoc } from "firebase/firestore";
 import { Firebase } from "../../utils/firebase";
 import logo from "../../assets/logo.png";
+
 const Navbar = () => {
   const [email, setEmail] = useState(localStorage.getItem("email"));
   const [username, setUsername] = useState("");
+  const [menuOpen, setMenuOpen] = useState(false); // State for toggling mobile menu
   const db = Firebase();
 
   useEffect(() => {
-    const displayUser = async () => {
+    const fetchUser = async () => {
       if (email) {
-        const docSnap = await getDoc(doc(db, "users", email));
-        if (docSnap.exists()) {
-          setUsername(docSnap.data().name.toUpperCase());
+        try {
+          const docSnap = await getDoc(doc(db, "users", email));
+          if (docSnap.exists()) {
+            setUsername(docSnap.data().name.toUpperCase());
+          }
+        } catch (error) {
+          console.error("Error fetching user data:", error);
         }
       }
     };
-    displayUser();
-  }, [email, db, username]);
+    fetchUser();
+  }, [email, db]);
 
-  const logout = () => {
+  const handleLogout = () => {
     localStorage.clear();
     setEmail(null);
+    setUsername("");
+    window.location.href = "/";
   };
 
   return (
     <div id="navbarParent">
       <nav id="navBar">
-        <link
-          rel="stylesheet"
-          href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css"
-          integrity="sha384-QYIZto+st3yW+o8+5OHfT6S482Zsvz2WfOzpFSXMF9zqeLcFV0/wlZpMtyFcZALm"
-          crossOrigin="anonymous"
-        />
         <div id="container">
-          <button
-            id="appName"
-            onClick={() => {
-              window.location.href = "/";
-            }}
-          >
-            <img src={logo} alt="" />
+          <button id="appName" onClick={() => (window.location.href = "/")}>
+            <img src={logo} alt="Skill Select Logo" />
           </button>
-          <div className="nameRender" id="itemBtn">
-            {username === "" ? "" : `HEY ${username} !`}
+          <div id="userGreeting" className="nameRender">
+            {username && `Hey, ${username}!`}
           </div>
-          <div id="navbarLinks">
+          <button
+            id="hamburger"
+            onClick={() => setMenuOpen((prev) => !prev)} // Toggle menu
+          >
+            ☰
+          </button>
+          <div id="navbarLinks" className={menuOpen ? "mobile-active" : ""}>
             <ul id="list">
-              <li id="item">
+              <li>
                 <button
                   id="itemBtn"
-                  onClick={() => {
-                    window.location.href = "/myList";
-                  }}
+                  onClick={() => (window.location.href = "/myList")}
                 >
                   Shortlisted
                 </button>
               </li>
-              <li id="item">
+              <li>
                 <button
                   id="itemBtn"
-                  onClick={() => {
-                    window.location.href = "/candidates";
-                  }}
+                  onClick={() => (window.location.href = "/candidates")}
                 >
                   All Candidates
                 </button>
               </li>
-              <li id="item">
-                {email ? (
-                  <button
-                    id="itemBtn"
-                    onClick={() => {
-                      logout();
-                    }}
-                  >
+              {email ? (
+                <li>
+                  <button id="itemBtn" onClick={handleLogout}>
                     Logout
                   </button>
-                ) : (
-                  <button
-                    id="itemBtn"
-                    onClick={() => {
-                      window.location.href = "/login";
-                    }}
-                  >
-                    Login
-                  </button>
-                )}
-              </li>
-              {email ? (
-                ""
-              ) : (
-                <li id="item">
-                  <button
-                    id="itemBtn"
-                    onClick={() => {
-                      window.location.href = "/signup";
-                    }}
-                  >
-                    Signup
-                  </button>
                 </li>
+              ) : (
+                <>
+                  <li>
+                    <button
+                      id="itemBtn"
+                      onClick={() => (window.location.href = "/login")}
+                    >
+                      Login
+                    </button>
+                  </li>
+                  <li>
+                    <button
+                      id="itemBtn"
+                      onClick={() => (window.location.href = "/signup")}
+                    >
+                      Signup
+                    </button>
+                  </li>
+                </>
               )}
             </ul>
           </div>
